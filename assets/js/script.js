e412cdab95fb1b228b16442fa89fe5423429418d
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2020-12-19 23:02:06 -0700

    finished the restaurant and brewery display functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,36 @@ var getBreweries = function() {
             breweries = data
             console.log(breweries);
         })
+        .then(function() {
+            setTimeout(displayBreweryHandler(), 2000);
+        })
     })
+    
 }
 
 //Get array of restaurants
 var getRestaurants = function() {
-    fetch("https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + long + "&apikey=510b377da1e68430bb8e2db41707b969")
+    var almostSearchableCity = city.toLowerCase();
+    var searchableCity = almostSearchableCity.replace(/ /g, "%20");
+    fetch("https://api.opencagedata.com/geocode/v1/json?q=" + searchableCity + "&key=7d27afefb2e045c994d1e56220e4dc26")
     .then(function(response) {
         response.json()
         .then(function(data) {
-            restaurants = data.restaurants
-            console.log(restaurants);
+            lat = data.results[0].geometry.lat;
+            long = data.results[0].geometry.lng;
+        })
+        .then(function() {
+            fetch("https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + long + "&apikey=510b377da1e68430bb8e2db41707b969")
+            .then(function(response) {
+                response.json()
+                .then(function(data) {
+                    restaurants = data.restaurants
+                    console.log(restaurants);
+                })
+                .then(function() {
+                    setTimeout(displayRestaurantHandler(), 2000);
+                })
+            })
         })
     })
 }
@@ -58,6 +77,7 @@ var errorCallback = function(error) {
 
 //function to handle search bar
 var searchHandler = function() {
+    resultsArea.classList.remove("hidden");
     city = searchInput.value
     if (document.querySelector("#restaurant-checkbox").checked) {
         getRestaurants();
@@ -65,7 +85,6 @@ var searchHandler = function() {
     }
     if (document.querySelector("#brewery-checkbox").checked) {
         getBreweries();
-        displayBreweryHandler();
     } else if (!(document.querySelector("#restaurant-checkbox").checked) && !(document.querySelector("#brewery-checkbox").checked)) {
         M.toast({html: 'Please select at least one option!', classes: 'rounded'})
     };
@@ -74,18 +93,34 @@ var searchHandler = function() {
 //function to handle displaying search results
 var displayRestaurantHandler = function() {
     console.log('worked')
-    var restaurantList = document.createElement("ul");
-    var restaurantsTag = document.createElement("h5");
-    restaurantsTag.textContent = "Restaurants Near You: "
-    resultsArea.append(restaurantsTag);
+    var currentRestaurantList = document.querySelector("#restaurant-list");
+    var newRestaurantsList = document.createElement("ul");
+    newRestaurantsList.setAttribute("id", "restaurant-list");
+    currentRestaurantList.replaceWith(newRestaurantsList);
+    console.log(restaurants.length);
+
+    for (i = 0; i < restaurants.length; i++) {
+        var restaurantItem = document.createElement("a");
+        restaurantItem.setAttribute("class", "list-item");
+        restaurantItem.textContent = restaurants[i].restaurant.name;
+        newRestaurantsList.appendChild(restaurantItem);
+    }
 }
 
 var displayBreweryHandler = function() {
     console.log('also worked')
-    var breweryList = document.createElement("ul");
-    var breweryTag = document.createElement("h5");
-    breweryTag.textContent = "Breweries Near You: "
-    resultsArea.append(breweryTag);
+    var currentBreweryList = document.querySelector("#brewery-list")
+    var newBreweryList = document.createElement("ul");
+    newBreweryList.setAttribute("id", "brewery-list");
+    currentBreweryList.replaceWith(newBreweryList);
+    console.log(breweries.length)
+
+    for (i = 0; i < breweries.length; i++) {
+        var breweryItem = document.createElement("a");
+        breweryItem.setAttribute("class", "list-item");
+        breweryItem.textContent = breweries[i].name;
+        newBreweryList.appendChild(breweryItem);
+    }
 }
 
 
